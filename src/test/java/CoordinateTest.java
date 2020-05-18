49d9dd415798381db6b9e993b05616893a8c0b6f
vishnupriyaRamanathan <rvishnu97@gmail.com>
2020-05-18 18:54:40 +0530

    Added test case where overriding hashcode method is checked

--- src/test/java/CoordinateTest.java
+++ src/test/java/CoordinateTest.java
@@ -1,12 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class CoordinateTest {
+    HashMap<Coordinate, String> hashMap = new HashMap<>();
+
+    @Before
+    public void setUp() {
+        Coordinate coordinateOne = new Coordinate(1, 1);
+        Coordinate coordinateTwo = new Coordinate(1, 1);
+
+        hashMap.put(coordinateOne, "One");
+        hashMap.put(coordinateTwo, "Two");
+    }
+
     @Test
     public void shouldCheckIfTwoCoordinatesAreEqual() {
-        Coordinate coordinateOne = new Coordinate(1, 1 );
-        Coordinate coordinateTwo = new Coordinate(1,1);
+        Coordinate coordinateOne = new Coordinate(1, 1);
+        Coordinate coordinateTwo = new Coordinate(1, 1);
         assertEquals(coordinateOne, coordinateTwo);
+    }
+
+    @Test
+    public void shouldCheckIfHashCodeIsValidated() {
+        Coordinate coordinate = new Coordinate(1, 1);
+        assertEquals("Two", hashMap.get(coordinate));
+        assertNotEquals("One", hashMap.get(coordinate));
     }
 }
